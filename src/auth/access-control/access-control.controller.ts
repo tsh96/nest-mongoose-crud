317df7f3ec4cb6e507e1ae7b0c6006bd2fe9a9ed
tsh96 <tansiahow1996@gmail.com>
2020-11-21 13:50:19 +0800

    remove role guard from access control controller

--- src/auth/access-control/access-control.controller.ts
+++ src/auth/access-control/access-control.controller.ts
@@ -1,15 +1,13 @@
-import { ParseArrayPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { ParseArrayPipe, ParseIntPipe } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { CrudController } from '../../crud/crud.controller';
 import { Crud } from '../../crud/crud.decorator';
 import { AuthResource } from '../resource.decorator';
-import { RolesGuard } from '../roles.guard';
 import { AccessControlService } from './access-control.service';
 import { FilterAccessControlDto } from './dto/filter-access-control.dto';
 
 @ApiBearerAuth()
 @AuthResource('access-control')
-@UseGuards(RolesGuard)
 @Crud('access-control', { crudService: AccessControlService, ParseArrayPipe, ParseIntPipe })
 export class AccessControlController extends CrudController<AccessControlService, FilterAccessControlDto>{
   constructor(readonly service: AccessControlService) {
